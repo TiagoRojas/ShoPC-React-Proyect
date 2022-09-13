cfc9c6aac168c86cb30ae531ca0768414391e584
Tiago Rojas <rojastiago04@gmail.com>
2022-09-13 16:56:57 -0300

    ItemCount added

---
+++ src/Components/ItemCount.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react"
+
+const ItemCount = ({initial, stock, addCart }) => {
+    const [count, setCount] = useState(initial)
+
+    const addOne = () => {
+        if(count < stock)setCount(count + 1);
+    }
+    const subOne = () => {
+        if(count > stock)setCount(count - 1);
+    }
+    return(
+        <>
+            <div className="amountContainer">
+                <button onClick={subOne} className="amountButton">-</button>
+                <div className="amount">{count}</div>
+                <button onClick={addOne} className="amountButton">+</button>
+                <button onClick={() => addCart(count)} className="submitButton">Añadir al carrito</button>
+            </div>
+        </>
+    )
+}
+
+export default ItemCount
--- src/Components/ItemListContainer/ItemListContainer.jsx
+++ src/Components/ItemListContainer/ItemListContainer.jsx
@@ -1,10 +1,14 @@
-
+import ItemCount from "../ItemCount"
 
 const ItemListContainer = ({greeting}) => {
+    const addCart = (count) => {
+        console.log(`Se agregan ${count} productos`);
+      }
     return (
     <>
         <a/>
         <h1 style={Styles.h1}>{greeting}</h1>
+        <ItemCount initial={1} stock={10} addCart={addCart}/>
     </>
     )
 }
